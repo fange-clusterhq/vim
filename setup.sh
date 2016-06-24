acf010f74aa193b4e68673b21f589219f3440426
Eric Fang <eric@clusterhq.com>
2016-06-24 12:52:12 -0700

    setup scripe updated

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 git config --global core.editor "vim"
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rm -rf $HOME/.config/nvim
 mkdir -p $HOME/.config
-ln -s $SRC_DIR/.vim $HOME/.config/nvim
-ln -s $SRC_DIR/.vimrc $HOME/.config/nvim/init.vim
+cp -r $SRC_DIR/.vim $HOME/.config/nvim
+cp -r $SRC_DIR/.vimrc $HOME/.config/nvim/init.vim
