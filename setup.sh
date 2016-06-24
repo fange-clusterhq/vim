1133b40240365d412763bf30682405c914ef7733
Eric Fang <eric@clusterhq.com>
2016-06-24 12:45:27 -0700

    Add link to neovim into setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 git config --global core.editor "vim"
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p $HOME/.config
+ln -s $SRC_DIR/.vim $HOME/.config/nvim
+ln -s $SRC_DIR/.vimrc $HOME/.config/nvim/init.vim
