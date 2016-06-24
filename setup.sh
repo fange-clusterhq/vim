4a882c91011f8b0a0a0648ffdc5c7e246a933a69
Eric Fang <eric@clusterhq.com>
2016-06-24 12:59:29 -0700

    added vim-plug in line to setup.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ rm -rf $HOME/.config/nvim
 mkdir -p $HOME/.config
 cp -r $SRC_DIR/.vim $HOME/.config/nvim
 cp -r $SRC_DIR/.vimrc $HOME/.config/nvim/init.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
